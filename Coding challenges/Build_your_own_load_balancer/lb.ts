57c608e9163983bb75202474aba3575bf6fd7280
Vedant Gonnade <vedantgonnade@outlook.com>
2026-02-01 15:37:33 +0100

    forwarded load balancer request to single backend server

--- Coding challenges/Build_your_own_load_balancer/lb.ts
+++ Coding challenges/Build_your_own_load_balancer/lb.ts
@@ -1,8 +1,11 @@
 import express from "express";
+import axios from "axios";
 
 const app = express();
 const PORT = 8080;
 const HOST = "localhost";
+
+const BACKEND_SERVER = "http://localhost:3000";
 
 app.use((req, res, next) => {
     console.log(`Received request from ${req.ip}`);
@@ -14,12 +17,24 @@ app.use((req, res, next) => {
     next();
 });
 
-app.get("/", (req, res) => {
-    res.send("Load balancer received your request");
+app.use(async (req, res) => {
+    try {
+        const response = await axios({
+            method: req.method,
+            url: `${BACKEND_SERVER}${req.url}`,
+            headers: req.headers,
+            data: req.body
+        });
+        res.status(response.status).set(response.headers).send(response.data);
+    }
+    catch (error) {
+        res.status(500).send("Internal Server Error");
+    }
 });
 
 app.listen(PORT, HOST, () => {
     console.log(`Load balancer running at http://${HOST}:${PORT}/`);
+    console.log(`Forwarding requests to ${BACKEND_SERVER}`);
 });
 
 export { app };
